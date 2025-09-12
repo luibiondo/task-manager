d0896a8ddc63fb85cf2890d292e863416caa922f
Luigi Biondo <luigibiondo2207@gmail.com>
2025-09-12 16:50:04 +0000

    Changing the component home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 'use client'
-import { Button } from '@/components/Button'
 import { Column } from '@/components/Column/column'
-import { useState } from 'react'  
+
 
 export default function Home() {
   return (
     <main className='bg-black p-4'>
       <div className='w-full h-screen bg-gradient-to-bl from-violet-500 to-fuchsia-500 flex gap-4 p-6'>
-        {/* CATCH THE COLUMN FROM IMPORT */}
+        
         <Column title="Hoje"></Column>
         <Column title="Esta semana"></Column>
         <Column title="Mais tarde"></Column>
       </div>
     </main>
-  );
+  )
 }
-
--- src/components/Column/column.tsx
+++ src/components/Column/column.tsx
@@ -6,14 +6,13 @@ interface IColumnProps {
 
 function Column({ title }: IColumnProps) {
   return (
-    <div className="bg-neutral-900 rounded-xl p-4 w-64 ml-49 shadow-lg">
-      {/* COLUMN HEADER */}
+    <div className="bg-neutral-900 rounded-xl p-4 w-64 ml-35 shadow-lg">
+      
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-white font-semibold">{title}</h2>
         <button className="text-white text-xl">+</button>
       </div>
 
-      {/* EXAMPLES OF CARDS */}
       <div className="space-y-2">
         <Card text="Exemplo de card 1" />
         <Card text="Exemplo de card 2" />
