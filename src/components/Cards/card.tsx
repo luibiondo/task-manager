78b2e05ebd759ec6517b4de780231e6708ef31ee
Luigi Biondo <luigibiondo2207@gmail.com>
2025-09-12 16:58:45 +0000

    Testando o hover

--- src/components/Cards/card.tsx
+++ src/components/Cards/card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 function Card({ text }: CardProps) {
   return (
-    <div className="bg-neutral-800 text-white p-3 rounded-lg shadow flex justify-center">
+    <div className="bg-neutral-800 text-white p-3 rounded-lg shadow flex justify-center hover:bg-gradient-to-bl from-violet-500 to-fuchsia-500">
       {text}
     </div>
   )
